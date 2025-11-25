6895ff268f65e7e9c6efdb2927fb0d665f20e45b
Alexander Selivanov <selivandex@gmail.com>
2025-11-25 20:39:24 +0700

    fix errors

---
+++ internal/adapters/exchanges/factory.go
@@ -0,0 +1,29 @@
+package exchanges
+
+import (
+	"prometheus/internal/domain/exchange_account"
+	"prometheus/pkg/crypto"
+)
+
+// SystemCredential holds credentials for system-wide market data collection
+type SystemCredential struct {
+	Exchange   exchange_account.ExchangeType
+	APIKey     string
+	Secret     string
+	Passphrase string
+	Testnet    bool
+	Market     MarketType
+}
+
+// Factory creates exchange clients for user accounts
+type Factory interface {
+	CreateClient(account *exchange_account.ExchangeAccount, encryptor *crypto.Encryptor) (Exchange, error)
+	GetClient(exchangeType exchange_account.ExchangeType) (Exchange, error)
+	ListExchanges() []exchange_account.ExchangeType
+}
+
+// CentralFactory creates exchange clients for centralized market data collection
+type CentralFactory interface {
+	GetClient(exchange string) (Exchange, error)
+	ListExchanges() []string
+}
